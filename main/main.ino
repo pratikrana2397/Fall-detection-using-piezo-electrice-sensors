900fea4c096e87db786d5f744ac89e06b8b54e43
Divyansh Gaba <divyansh@hackerearth.com>
2018-09-16 07:39:01 +0530

    supports 5 tiles, need 3 to trigger now

--- main/main.ino
+++ main/main.ino
@@ -4,6 +4,7 @@ const int knockSensor0 = A0; // the piezo is connected to analog pin 0
 const int knockSensor1 = A1;
 const int knockSensor2 = A2;
 const int knockSensor3 = A3;
+const int knockSensor4 = A4;
 const int threshold = 200;  // threshold value to decide when the detected sound is a knock or not
 
 
@@ -12,6 +13,9 @@ int sensorReading0 = 0;
 int sensorReading1 = 0;
 int sensorReading2 = 0;
 int sensorReading3 = 0;
+int sensorReading4 = 0;
+int thresh = 3;
+int total = 5;
 int ledState = LOW;         // variable used to store the last LED status, to toggle the light
 int buzzState = LOW;
 
@@ -23,9 +27,13 @@ void trigger()
       ledState=!ledState;
       digitalWrite(ledPin, ledState);
       buzzState=!buzzState;
-      digitalWrite(buzzPin,HIGH);
+      digitalWrite(buzzPin,buzzState);
       delay(100);
     }
+    ledState=LOW;
+    digitalWrite(ledPin, ledState);
+    buzzState=LOW;
+    digitalWrite(buzzPin,buzzState);
 }
 
 void setup() {
@@ -40,8 +48,24 @@ void loop() {
   sensorReading1 = analogRead(knockSensor1);
   sensorReading2 = analogRead(knockSensor2);
   sensorReading3 = analogRead(knockSensor3);
-  bool fall = (sensorReading0 < 200) && (sensorReading1 < 200) && (sensorReading2 < 200) &&(sensorReading3 < 200); 
-  if(fall)
+  sensorReading4 = analogRead(knockSensor4);
+  int count = 0;
+  if(sensorReading0 < 200){
+    count++;
+  }
+  if(sensorReading1 < 200){
+    count++;
+  }
+  if(sensorReading2 < 200){
+    count++;
+  }
+  if(sensorReading3 < 200){
+    count++;
+  }
+  if(sensorReading4 < 200){
+    count++;
+  }
+  if(count >= thresh)
   {  
     trigger();
   }
@@ -51,6 +75,8 @@ void loop() {
   Serial.print(" ");
   Serial.print(sensorReading2);
   Serial.print(" ");
-  Serial.println(sensorReading3);
+  Serial.print(sensorReading3);
+  Serial.print(" ");
+  Serial.println(sensorReading4);
   delay(100);  //   delay to avoid overloading the serial port buffer
 }
